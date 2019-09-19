ed1732b69eb4ce213c053c32287ad84cb8ae83b0
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-09-19 19:41:24 +0100

    start setting up

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return <div>Landing</div>;
+  // <div className="landing contain">
+  //   {/* <div className="landing__logo">
+  //     <img src={logo} alt="logo" className="landing__logo-image" />
+  //   </div> */}
+  //   <div className="landing__slogan mt-4">
+  //     <p className="landing__slogan-text lead-text">
+  //       Αλεξάνδρειο Τεχνολογικό Εκπαιδευτικό Ίδρυμα Θεσσαλονίκης - Career
+  //       Portal
+  //     </p>
+  //     {/* <Link to="/register" className="btn btn-lg btn-info mr-2">
+  //         Sign Up
+  //       </Link> */}
+  //     {this.props.auth.isAuthenticated ? (
+  //       <Link to="/dashboard">
+  //         <button className="button submit-btn">Go to Dashboard</button>
+  //       </Link>
+  //     ) : (
+  //       <Fragment>
+  //         <Link to="/register">
+  //           <button className="button submit-btn mr1">Sign Up</button>
+  //         </Link>
+  //         <Link to="/login">
+  //           <button className="button back-btn">Login</button>
+  //         </Link>
+  //       </Fragment>
+  //     )}
+  //   </div>
+  // </div>
+};
+
+export default Landing;
