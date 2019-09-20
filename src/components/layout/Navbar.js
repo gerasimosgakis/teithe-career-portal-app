50f96d8ba1738a5326ea14db4a0738d418f8bae5
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-09-20 19:29:55 +0100

    feat: set up redux and start auth actions

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,123 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-sm navbar-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          <i className="fas fa-code"></i> Careers
-        </Link>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a href="!#">Developers</a>
-          </li>
-          <li className="nav-item">
-            <a href="/register">Register</a>
-          </li>
-          <li className="nav-item">
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    //   <nav className="navbar navbar-expand-sm navbar-dark">
-    //   <div className="container">
-    //     <Link className="navbar-brand" to="/">
-    //       Careers
-    //     </Link>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-toggle="collapse"
-    //       data-target="#mobile-nav"
-    //       aria-controls="navbarTogglerDemo01"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon" />
-    //     </button>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
 
-    //     <div className="collapse navbar-collapse" id="mobile-nav">
-    //       <ul className="navbar-nav mr-auto">
-    //         {this.props.auth.isAuthenticated ? (
-    //           <Fragment>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/profiles">
-    //                 {" "}
-    //                 Graduates
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/feed">
-    //                 {" "}
-    //                 Post Feed
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/chat">
-    //                 {" "}
-    //                 Chat
-    //               </Link>
-    //             </li>
-    //           </Fragment>
-    //         ) : (
-    //           <li className="nav-item">
-    //             <Link className="nav-link" to="/profiles">
-    //               {" "}
-    //               Graduates
-    //             </Link>
-    //           </li>
-    //         )}
-    //       </ul>
+    this.state = {
+      isLoading: false,
+      isAuthenticated: false
+    };
+  }
 
-    //       <ul className="navbar-nav ml-auto">
-    //         {this.props.auth.isAuthenticated ? (
-    //           <Fragment>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/dashboard">
-    //                 <img
-    //                   className="rounded-circle d-none d-md-block"
-    //                   src={auth.user.avatar}
-    //                   alt="avatar"
-    //                 />
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a
-    //                 href="_target"
-    //                 rel="noopener noreferrer"
-    //                 className="nav-link mt-half"
-    //                 onClick={this.onLogout}
-    //               >
-    //                 Logout
-    //               </a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/settings">
-    //                 <i className="fas fa-cog mt-half" />
-    //               </Link>
-    //             </li>
-    //           </Fragment>
-    //         ) : (
-    //           <Fragment>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/register">
-    //                 Sign Up
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to="/login">
-    //                 Login
-    //               </Link>
-    //             </li>
-    //           </Fragment>
-    //         )}
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
-  );
-};
+  render() {
+    return <div>Navbar</div>;
+  }
+}
 
 export default Navbar;
