fdcc07ca8bd5cc3b824b6fcad9d3d16fe968e232
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-11-26 19:13:28 +0000

    fixes for build

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import alert from "./alert";
 import auth from "./authReducer";
 import profiles from "./profileReducer";
 import posts from "./postReducer";
 
 export default combineReducers({
-  alert,
   auth,
   profiles,
   posts
