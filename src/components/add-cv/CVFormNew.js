0466cc29e599bd623111372ae8f63a82f9b7a25a
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-11-30 20:47:10 +0000

    add spinner

--- src/components/add-cv/CVFormNew.js
+++ src/components/add-cv/CVFormNew.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import config from "../../config";
 import { s3Upload } from "../../shared/functions/aws";
 import { connect } from "react-redux";
+import Spinner from "../shared/Spinner";
 
 class CVFormNew extends Component {
   constructor(props) {
@@ -9,7 +10,8 @@ class CVFormNew extends Component {
 
     this.state = {
       file: null,
-      user: this.props.auth.user.username
+      user: this.props.auth.user.username,
+      loading: false
     };
   }
 
@@ -20,7 +22,7 @@ class CVFormNew extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    this.setState({ loading: true });
     if (this.state.file && this.state.file.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
         `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
@@ -32,8 +34,10 @@ class CVFormNew extends Component {
     try {
       await s3Upload(this.state.file, this.state.user);
       // file = null;
-      this.setState({ file: null });
+      this.setState({ file: null, loading: false });
+      alert("saved");
     } catch (error) {
+      this.setState({ loading: false });
       alert(error);
     }
   };
@@ -41,14 +45,18 @@ class CVFormNew extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="file"
-            className="inputfile mr2"
-            onChange={this.handleFileChange}
-          />
-          <button className="button submit-btn">Submit</button>
-        </form>
+        {this.state.loading ? (
+          <Spinner></Spinner>
+        ) : (
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="file"
+              className="inputfile mr2"
+              onChange={this.handleFileChange}
+            />
+            <button className="button submit-btn">Submit</button>
+          </form>
+        )}
       </div>
     );
   }
