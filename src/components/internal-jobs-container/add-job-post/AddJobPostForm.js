a1fe4bcaddef3e2384afdf4f31890ca2d30fd80d
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-12-08 18:01:45 +0000

    add modals for adding and editing internal jobs

---
+++ src/components/internal-jobs-container/add-job-post/AddJobPostForm.js
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addInternalJob } from "../../../redux/actions/internalJobActions";
+import TextFieldGroup from "../../shared/TextFieldGroup";
+import TextAreaFieldGroup from "../../shared/TextAreaFieldGroup";
+
+class AddJobPostForm extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props);
+
+    const {
+      id,
+      currentJobIndex,
+      title,
+      recruiter,
+      location,
+      min_salary,
+      max_salary,
+      description
+    } = this.props;
+
+    this.state = {
+      currentJobIndex,
+      id,
+      title,
+      recruiter,
+      location,
+      min_salary,
+      max_salary,
+      description
+    };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const userId = this.props.auth.user.username;
+    console.log(this.state.id);
+    // this.props.addInternalJob({
+    //   user_id: userId,
+    //   title: this.state.title,
+    //   recruiter: this.state.recruiter,
+    //   min_salary: this.state.min_salary,
+    //   max_salary: this.state.max_salary,
+    //   description: this.state.description,
+    //   location: this.state.location
+    // });
+  };
+
+  /** Get Derived State From Props method
+   *  We use this method so we can update the data in the form every time we choose a
+   *  different job to edit
+   */
+  static getDerivedStateFromProps(props, current_state) {
+    console.log(props, current_state);
+    if (current_state.currentJobIndex !== -1 && props.currentJobIndex < 0) {
+      return {
+        id: props.id,
+        title: "",
+        recruiter: "",
+        location: "",
+        min_salary: null,
+        max_salary: null,
+        description: ""
+      };
+    } else if (
+      props.currentJobIndex !== -1 &&
+      props.currentJobIndex !== current_state.currentJobIndex
+    ) {
+      // Check if we are using a different job and update state if we do
+      return {
+        id: props.id,
+        title: props.title,
+        recruiter: props.recruiter,
+        location: props.location,
+        min_salary: props.min_salary,
+        max_salary: props.max_salary,
+        description: props.description,
+        currentJobIndex: props.currentJobIndex
+      };
+    } else {
+      return null;
+    }
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <small className="small-text">* = required fields</small>
+        <form className="mt1" onSubmit={this.onSubmit}>
+          <div className="form__field-label">* Title</div>
+          <TextFieldGroup
+            placeholder="* Title"
+            name="title"
+            value={this.state.title}
+            required
+            onChange={this.onChange}
+          />
+          <div className="form__field-label">* Recruiter</div>
+          <TextFieldGroup
+            placeholder="* Recruiter"
+            name="recruiter"
+            value={this.state.recruiter}
+            required
+            onChange={this.onChange}
+          />
+          <div className="form__field-label">* Location</div>
+          <TextFieldGroup
+            placeholder="* Location"
+            name="location"
+            value={this.state.location}
+            required
+            onChange={this.onChange}
+          />
+          <div className="form__field-label">Minimum Salary</div>
+          <TextFieldGroup
+            placeholder="Minimum Salary"
+            name="min_salary"
+            value={this.state.min_salary}
+            type="number"
+            onChange={this.onChange}
+          />
+          <div className="form__field-label">Maximum Salary</div>
+          <TextFieldGroup
+            placeholder="Maximum Salary"
+            name="max_salary"
+            value={this.state.max_salary}
+            type="number"
+            onChange={this.onChange}
+          />
+          <div className="form__field-label">Description</div>
+          <TextAreaFieldGroup
+            placeholder="Description"
+            name="description"
+            value={this.state.description}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-info btn-block mt-4"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    auth: state.auth
+  };
+};
+
+export default connect(mapStateToProps, {
+  addInternalJob
+})(AddJobPostForm);
--- src/components/internal-jobs-container/internal-jobs-search/InternalJobs.js
+++ src/components/internal-jobs-container/internal-jobs-search/InternalJobs.js
@@ -4,10 +4,38 @@ import { withRouter } from "react-router-dom";
 import { getInternalJobs } from "../../../redux/actions/internalJobActions";
 import Spinner from "../../shared/Spinner";
 import InternalJobItem from "./InternalJobItem";
+import AddJobPostForm from "../add-job-post/AddJobPostForm";
 
 class InternalJobs extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentJobIndex: -1,
+      title:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].title
+          : null,
+      recruiter:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].recruiter
+          : null,
+      location:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].location
+          : null,
+      min_salary:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].min_salary
+          : null,
+      max_salary:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].max_salary
+          : null,
+      description:
+        this.props.internalJobs && this.props.internalJobs.length > 0
+          ? this.props.internalJobs[0].description
+          : null
+    };
   }
 
   componentDidMount() {
@@ -21,8 +49,31 @@ class InternalJobs extends Component {
     if (internalJobs === null || loading) {
       content = <Spinner />;
     } else {
-      content = internalJobs.map(job => (
-        <InternalJobItem job={job}></InternalJobItem>
+      content = internalJobs.map((job, index) => (
+        <div>
+          <InternalJobItem job={job}></InternalJobItem>
+          <button
+            className="icon-button icon-button--small"
+            data-toggle="modal"
+            data-target="#editModal"
+            onClick={() => {
+              // this.currentExperienceIndex = index;
+              this.setState({
+                id: job.id,
+                title: job.title,
+                recruiter: job.recruiter,
+                location: job.location,
+                min_salary: job.min_salary,
+                max_salary: job.max_salary,
+                description: job.description,
+                currentJobIndex: index
+              });
+              // console.log(this.currentExperienceIndex);
+            }}
+          >
+            <i className="fas fa-edit"></i>
+          </button>
+        </div>
       ));
     }
     return (
@@ -34,6 +85,44 @@ class InternalJobs extends Component {
           </div>
         )}
         <div className="search-jobs__results-main">{content}</div>
+        {/* Job Edit Modal */}
+        <div className="modal fade" id="editModal" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Edit Job</h4>
+                <button type="button" className="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+              <div className="modal-body">
+                {this.state.currentJobIndex >= 0 ? (
+                  <AddJobPostForm
+                    id={this.state.id}
+                    title={this.state.title}
+                    recruiter={this.state.recruiter}
+                    location={this.state.location}
+                    min_salary={this.state.min_salary}
+                    max_salary={this.state.max_salary}
+                    description={this.state.description}
+                    currentJobIndex={this.state.currentJobIndex}
+                  />
+                ) : (
+                  <AddJobPostForm title={""} />
+                )}
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
